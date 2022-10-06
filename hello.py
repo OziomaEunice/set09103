9a828ea2a9680333699fd947c7bea8cf9b750188
OziomaEunice <onwubikooziomaeunice@gmail.com>
2022-10-06 16:41:19 +0100

    error message

--- hello.py
+++ hello.py
@@ -32,5 +32,10 @@ def private():
 def login():
     return "Now we would get username & password" # This is the login page
 
+# Create a (custom) error message when a page is not found
+@app.errorhandler(404)
+def page_not_found(error):
+    return "Couldn't find the page you requested", 404
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
