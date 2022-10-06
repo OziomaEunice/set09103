52051b132f2fdda02cbecaff766ae21ce08b2908
OziomaEunice <onwubikooziomaeunice@gmail.com>
2022-10-06 15:59:27 +0100

    addition

--- hello.py
+++ hello.py
@@ -2,5 +2,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello Napier'
+def root():
+    return "The default, 'root' root"
+
+@app.route("/hello/")
+def hello():
+    return "Hello Napier!!!  :D"
+
+@app.route("/goodbye/")
+def goodbye():
+    return "Goodbye cruel world  :("
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
